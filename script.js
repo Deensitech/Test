bdd7f2ef55b0b522910a050a507a4ac3931316e1
deensitech <lawaltaye301@gmail.com>
2024-05-22 18:31:06 +0100

    feat: loop continue and break

--- script.js
+++ script.js
@@ -419,3 +419,48 @@ if (markObject.bmi > johnObject.bmi) {
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weights repetition ${rep} 🏋️🏋️🏋️`);
 }
+
+//Loop Iteration...
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+  //Reading from jonas array
+  console.log(jonas[i], typeof jonas[i]);
+
+  //Filling types array
+  types[i] = typeof jonas[i];
+  types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const yearss = [1991, 2007, 1969, 2020];
+const agess = [];
+
+for (let i = 0; i < yearss.length; i++) {
+  agess.push(2037 - yearss[i]);
+}
+console.log(agess);
+
+//Continue and break
+console.log("---ONLY STRINGS---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log("---BREAK WITH NUMBER---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") break;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
